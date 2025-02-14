0e3b78e6b4b9733c0a136e7d20809f28f6bcc828
Mara <maracodes213@gmail.com>
2025-02-14 12:11:50 -0800

    strpie netlify

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-// Load Stripe Secret Key from environment variables
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const YOUR_DOMAIN = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 
@@ -19,9 +18,8 @@ export async function POST(req) {
       );
     }
 
-    // Convert cart items into Stripe's line item format
     const lineItems = cart.map((item) => ({
-      price: item.stripePriceId, // Stripe price ID
+      price: item.stripePriceId,
       quantity: 1, // Default to 1 per item
     }));
 
@@ -29,7 +27,7 @@ export async function POST(req) {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: lineItems,
-      mode: "subscription", // Ensure it matches your pricing model
+      mode: "subscription",
       phone_number_collection: { enabled: true },
       success_url: `${YOUR_DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${YOUR_DOMAIN}/checkout`,
